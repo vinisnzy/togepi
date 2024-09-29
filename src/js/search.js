727346eec530fbec346ed8c6f10bdc02021eef23
Vinícius de Paula <manoseilacara123@gmail.com>
2024-09-29 15:59:23 -0300

    added search system

---
+++ src/js/search.js
@@ -0,0 +1,33 @@
+const divSearch = document.querySelector(".input-wrapper")
+const search = document.querySelector(".input-wrapper input");
+const products = document.querySelectorAll(".products ul li");
+const notFound = document.querySelector("#notFound");
+
+search.addEventListener("input", () => {
+    for (let product of products) {
+        if (search.value != "") {
+            let name = product.querySelector(".productName");
+            name = name.textContent.toLowerCase();
+            let searchValue = search.value.toLowerCase();
+            if (!name.includes(searchValue)) {
+                product.style.display = "none";
+                let index = 0;
+                for (let product of products) {
+                    if (product.style.display == "none") {
+                        index++;
+                    }
+                }
+                if (index == products.length) {
+                    notFound.style.display = "flex";
+                }
+            } else {
+                product.style.display = "block";
+                notFound.style.display = "none";
+            }
+        } else {
+            for (let product of products) {
+                product.style.display = "block";
+            }
+        }
+    }
+});
